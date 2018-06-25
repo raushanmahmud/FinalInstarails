c536ce55e5733ce3d0f94d315901eb3074ac0fb6
Dagar <dagar@method.kz>
2018-06-25 15:27:23 +0600

    Implement like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,18 @@
 class LikesController < ApplicationController
 	
 	def create
-		
+		@post_id = params[:like][:post_id]
+		like = Like.create(user_id: current_user.id, post_id: @post_id)
+		@count = Post.find(@post_id).likes.count
+		respond_to do |format|
+			format.js {}
+		end
 	end
 
 	def destroy
 		@post_id = params[:like][:post_id]
 		Like.where(post_id: @post_id, user_id: current_user.id).first.destroy!
+		@count = Post.find(@post_id).likes.count
 		respond_to do |format|
 			format.js {}
 		end
